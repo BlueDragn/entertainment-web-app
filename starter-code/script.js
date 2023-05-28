7c9312ecceae3787ca3761fb21f6da22a17976e9
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-05-28 14:34:18 +0530

    added style and cards

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,13 +1,39 @@
 fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-  // Store the JSON data in a variable
-  const jsonData = data;
+    .then(response => response.json())
+    .then(data => {
+        // Store the JSON data in a variable
+        const jsonData = data;
 
-  // Process the JSON data
-  console.log(jsonData);
+        // Process the JSON data
+        console.log(jsonData);
 
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+
+        const showsCardsElement = document.querySelector(".cards");
+
+        jsonData.forEach((item) => {
+
+            //Create the card element
+            const cardElement = document.createElement("div");
+            cardElement.classList.add("card");
+            cardElement.style.backgroundImage = `url(${item.thumbnail.small})`;
+
+
+
+            // Add a heading element with the country's name
+            const nameElement = document.createElement("h2");
+            nameElement.textContent =item.title;
+            cardElement.appendChild(nameElement);
+
+            // Add an image element with the country's flag image URL
+      const thumbnailElement = document.createElement("img");
+      thumbnailElement.src = item.thumbnail.trending.small;
+      cardElement.appendChild(thumbnailElement);
+
+
+            showsCardsElement.appendChild(cardElement);
+        });
+
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
